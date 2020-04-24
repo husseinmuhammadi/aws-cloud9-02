e6b851946d185089f2c1d3e3455c891c2fe87a9b
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-04-24 10:37:51 +0430

    2

--- Applcaition.java
+++ Applcaition.java
@@ -4,5 +4,9 @@
 public class Applcaition {
 	public static void main(String[] args) {
     	System.out.println("Hello from AWS Cloud9!");
+
+    	Sample sample = new Sample();
+    	sample.test();
+
 	}
 }
